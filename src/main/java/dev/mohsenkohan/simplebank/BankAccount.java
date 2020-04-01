50bd59f91ef80a307fef9d62dc74266ea9264aa8
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-04-01 19:16:55 +0430

    Adding convenience methods to interface
    The BankAccount interface now contains a static method createSavingsWithDeposit
    and a default method isEmpty.

--- src/main/java/dev/mohsenkohan/simplebank/BankAccount.java
+++ src/main/java/dev/mohsenkohan/simplebank/BankAccount.java
@@ -17,4 +17,14 @@ public interface BankAccount extends Comparable<BankAccount> {
     void addInterest();
 
     String toString();
+
+    static BankAccount createSavingsWithDeposit(int acctNum, int amt) {
+        BankAccount bankAccount = new SavingsAccount(acctNum);
+        bankAccount.deposit(amt);
+        return bankAccount;
+    }
+
+    default boolean isEmpty() {
+        return getBalance() == 0;
+    }
 }
